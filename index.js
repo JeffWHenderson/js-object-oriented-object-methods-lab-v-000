65a26744c3e8046695d7a28e3808446740a6fbae
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-22 19:47:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ function BoardMember(name, city, job) {
   this.approve = function() {
     return "You can do that!"
   }
+  this.doCharity = function() {
+    return "I like to help people."
+  }
 }
